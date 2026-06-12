4fec37812eb4bc229827cee55956c4b338e2be88
nickeden4 <nick@therevenueclub.co.uk>
2026-06-12 13:33:50 +0100

    Add Garon Park proposal

---
+++ server.cjs
@@ -0,0 +1,44 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const root = __dirname;
+const port = Number(process.env.PORT || 4173);
+const host = "127.0.0.1";
+
+const types = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".js": "application/javascript; charset=utf-8",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+};
+
+const server = http.createServer((req, res) => {
+  const requestedPath = decodeURIComponent(new URL(req.url, `http://${host}`).pathname);
+  const filePath = path.join(root, requestedPath === "/" ? "index.html" : requestedPath);
+
+  if (!filePath.startsWith(root)) {
+    res.writeHead(403);
+    res.end("Forbidden");
+    return;
+  }
+
+  fs.readFile(filePath, (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      res.end("Not found");
+      return;
+    }
+
+    res.writeHead(200, {
+      "Content-Type": types[path.extname(filePath).toLowerCase()] || "application/octet-stream",
+    });
+    res.end(data);
+  });
+});
+
+server.listen(port, host, () => {
+  console.log(`Garon Park proposal running at http://${host}:${port}/`);
+});
